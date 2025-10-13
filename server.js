6c8b940dd743dd6975201bb92b50f4a12dacc444
Hamza Naveed Malik <hamzanaveedmalik@gmail.com>
2025-10-13 01:40:49 +0100

    fix: add x-user-id to CORS allowed headers and PATCH method

--- server.js
+++ server.js
@@ -81,8 +81,8 @@ app.use(cors({
         }
     },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-user-id']
 }));
 app.use(express.json());
 
